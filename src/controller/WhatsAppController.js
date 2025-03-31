6c8a02ba26cf261e10563fee7c9552d6999b3ab3
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-03-31 13:16:02 -0300

    feat: add events perfil contact

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,93 @@
 class WhatsAppController {
   constructor() {
     console.log("WhatsAppController ok");
+
+    this.elementsPrototype();
+    this.loadElements();
+    this.initEvents();
+  }
+
+  loadElements() {
+    this.el = {};
+
+    document.querySelectorAll("[id]").forEach((element) => {
+      this.el[Format.getCamelCase(element.id)] = element;
+    });
+  }
+
+  elementsPrototype() {
+    Element.prototype.hide = function () {
+      this.style.display = "none";
+    };
+
+    Element.prototype.show = function () {
+      this.style.display = "block";
+    };
+
+    Element.prototype.toggle = function () {
+      this.style.display = this.style.display === "none" ? "block" : "none";
+    };
+
+    Element.prototype.on = function (events, fn) {
+      events.split(" ").forEach((event) => {
+        this.addEventListener(event, fn);
+        console.log("clicou");
+      });
+    };
+    Element.prototype.css = function (styles) {
+      for (let name in styles) {
+        this.style[name] = styles = [name];
+      }
+    };
+
+    Element.prototype.addClass = function (name) {
+      this.classList.add(name);
+      return this;
+    };
+
+    Element.prototype.remove = function (name) {
+      this.classList.remove(name);
+      return this;
+    };
+
+    Element.prototype.toggleClass = function (name) {
+      this.classList.toggle(name);
+      return this;
+    };
+
+    Element.prototype.hasClass = function (name) {
+      return this.classList.contains(name);
+    };
+  }
+
+  initEvents() {
+    this.el.myPhoto.on("click", (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelEditProfile.show();
+      setTimeout(() => {
+        this.el.panelEditProfile.addClass("open");
+      }, 300);
+    });
+
+    this.el.btnNewContact.on("click", (e) => {
+      this.closeAllLeftPanel();
+      this.el.panelAddContact.show();
+      setTimeout(() => {
+        this.el.panelAddContact.addClass("open");
+      }, 300);
+    });
+
+    this.el.btnClosePanelEditProfile.on("click", (e) => {
+      this.el.panelEditProfile.removeClass("open");
+    });
+
+    this.el.btnClosePanelAddContact.on("click", (e) => {
+      this.el.panelAddContact.removeClass("open");
+    });
+  }
+
+  closeAllLeftPanel() {
+    this.el.panelAddContact.hide();
+    this.el.panelEditProfile.hide();
   }
 }

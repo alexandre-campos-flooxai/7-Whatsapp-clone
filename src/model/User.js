41e3e48abdb42c987102002170f78e2505abbede
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-04-03 18:18:59 -0300

    feat: updating contact list and conversation between user

--- src/model/User.js
+++ src/model/User.js
@@ -29,6 +29,13 @@ export class User extends Model {
     this._data.photo = value;
   }
 
+  get chatId() {
+    return this._data.chatId;
+  }
+  set chatId(value) {
+    this._data.chatId = value;
+  }
+
   getById(id) {
     return new Promise((s, f) => {
       User.findByEmail(id).onSnapshot((doc) => {
@@ -46,15 +53,33 @@ export class User extends Model {
     return Firebase.db().collection("/users");
   }
 
+  static getContactsRef(id) {
+    return User.getRef().doc(id).collection("contacts");
+  }
+
   static findByEmail(email) {
     return User.getRef().doc(email);
   }
 
   addContact(contact) {
-    return User.getRef()
-      .doc(this.email)
-      .collection("contacts")
+    return User.getContactsRef(this.email)
       .doc(btoa(contact.email))
       .set(contact.toJSON());
+  }
+
+  getContacts() {
+    return new Promise((s, f) => {
+      User.getContactsRef(this.email).onSnapshot((docs) => {
+        let contacts = [];
+
+        docs.forEach((doc) => {
+          let data = doc.data();
+          data.id = doc.id;
+          contacts.push(data);
+        });
+        this.trigger("contactschange", docs);
+        s(contacts);
+      });
+    });
   }
 }

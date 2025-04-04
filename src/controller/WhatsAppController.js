25bd5d303c1d82edae6e4a36200b582e251b1984
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-04-04 11:40:29 -0300

    feat: sending image from gallery as attachment

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -361,13 +361,12 @@ export class WhatsAppController {
 
     this.el.btnAttachPhoto.on("click", (e) => {
       this.el.inputPhoto.click();
-      console.log("photo");
+   
     });
 
     this.el.inputPhoto.on("change", (e) => {
-      console.log(this.el.inputPhoto.files);
       [...this.el.inputPhoto.files].forEach((file) => {
-        console.log(file);
+        Message.sendImage(this._contactActive.chatId,this._user.email,file);
       });
     });
 
--- src/model/Message.js
+++ src/model/Message.js
@@ -74,8 +74,8 @@ export class Message extends Model {
                    <div class="_3a5-b">
                        <div class="_1DZAH" role="button">
                            <span class="message-time">${Format.timeStampToTime(
-                            this.timeStamp
-                          )}</span>
+                             this.timeStamp
+                           )}</span>
 
                        </div>
                    </div>
@@ -86,7 +86,8 @@ export class Message extends Model {
        </div>`;
         break;
       case "image":
-        div.innerHTML = `<div class="_3_7SH _3qMSo  ">
+        div.innerHTML = `
+        <div class="_3_7SH _3qMSo  ">
         < class="KYpDv">
             <div>
                 <div class="_3v3PK" style="width: 330px; height: 330px;">
@@ -106,19 +107,14 @@ export class Message extends Model {
                             </div>
                         </div>
                     </div>
-                    <img src="#" class="_1JVSX message-photo" style="width: 100%; display:none">
+                    <img src="${this.content}" class="_1JVSX message-photo" style="width: 100%; display:none">
                     <div class="_1i3Za"></div>
-                </div>
-                <div class="message-container-legend">
-                    <div class="_3zb-j ZhF0n">
-                        <span dir="ltr" class="selectable-text invisible-space copyable-text message-text">Texto da foto</span>
-                    </div>
                 </div>
                 <div class="_2TvOE">
                     <div class="_1DZAH text-white" role="button">
                         <span class="message-time">${Format.timeStampToTime(
-                            this.timeStamp
-                          )}</span>
+                          this.timeStamp
+                        )}</span>
                     </div>
                 </div>
             </div>
@@ -131,6 +127,14 @@ export class Message extends Model {
             </span>
         </div>
     </div>`;
+    div.querySelector('.message-photo').on('load', e=>{
+        div.querySelector(".message-photo").show()
+        div.querySelector('._34Olu').hide()
+        div.querySelector('._34Olu').css({
+            height: 'auto'
+        })
+         
+    })
         break;
       case "document":
         div.innerHTML = `<div class="_3_7SH _1ZPgd  ">
@@ -167,8 +171,8 @@ export class Message extends Model {
         <div class="_3Lj_s">
             <div class="_1DZAH" role="button">
                 <span class="message-time">${Format.timeStampToTime(
-                    this.timeStamp
-                  )}</span>
+                  this.timeStamp
+                )}</span>
             </div>
         </div>
     </div>
@@ -239,8 +243,8 @@ export class Message extends Model {
             <div class="_27K_5">
                 <div class="_1DZAH" role="button">
                     <span class="message-time">${Format.timeStampToTime(
-                        this.timeStamp
-                      )}</span>
+                      this.timeStamp
+                    )}</span>
                 </div>
             </div>
         </div>
@@ -288,23 +292,60 @@ export class Message extends Model {
     return div;
   }
 
+  static sendImage(chatId, from, file) {
+    return new Promise((s, f) => {
+      let uploadTask = Firebase.hd()
+        .ref(from)
+        .child(Date.now() + "_" + file.name)
+        .put(file);
+
+      uploadTask.on(
+        "state_changed",
+        (e) => {
+          console.log("upload", e);
+        },
+        (err) => {
+          console.error(err);
+        },
+        () => {
+          Message.send(
+            chatId,
+            from,
+            "image",
+            uploadTask.snapshot.downloadURL
+          ).then(() => {
+            s();
+          });
+        }
+      );
+    });
+  }
+
   static send(chatId, from, type, content) {
     return new Promise((s, f) => {
-      Message.getRef(chatId).add({
-        content,
-        timeStamp: new Date(),
-        status: 'wait',
-        type,
-        from
-      }).then(result => {
-        result.parent.doc(result.id).set({
-          status: 'sent'
-        },{
-            merge:true
-        }).then(() => {
-            s()
+      Message.getRef(chatId)
+        .add({
+          content,
+          timeStamp: new Date(),
+          status: "wait",
+          type,
+          from,
+        })
+        .then((result) => {
+          result.parent
+            .doc(result.id)
+            .set(
+              {
+                status: "sent",
+              },
+              {
+                merge: true,
+              }
+            )
+            .then(() => {
+              s();
+            });
         });
-      });
     });
   }
 
